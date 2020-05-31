d37e229d3be64d7338c25115e3d333a6669294a1
Charlie Wolfsandle <charles.wolfsandle@ledcor.com>
2020-05-31 13:40:45 -0500

    Add project files.

---
+++ CW Exception Handling/Form1.cs
@@ -0,0 +1,73 @@
+﻿/* Charlie Wolfsandle
+   MS539 
+   5/22/2020
+   This program is to demonstrate the use of exception handling through the use of If..Else and Try / Catch */
+
+/* I think that this will take me approximately 30 minutes 5/22/2020 */
+
+/* It took me an hour to write since I wanted to add extra functionality and comments 5/22/2020*/
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace CW_Exception_Handling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result;
+            string str = textBox1.Text;
+            if (int.TryParse(str, out result)) /*tries to parse the contents of str as an integer*/
+            {
+                MessageBox.Show("This is a valid integer" + Environment.NewLine + "(in the IF statement)"); /*Environment.NewLine is what will add a newline to the message box*/
+            }
+            else
+            {
+                MessageBox.Show("This is NOT a valid integer" + Environment.NewLine + "(in the ELSE statement)");
+                MessageBox.Show("Please Try Again" + Environment.NewLine + "(Integers are whole numbers)");
+                textBox1.Clear(); /*clears the entry if it is not an integer to guide user to try again*/
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Boolean status = true;
+            string str2 = textBox2.Text;
+            try /*tries to parse entry as an integer*/
+            {
+                int.Parse(str2);
+            }
+            catch /*if the try fails, this will run*/
+            {
+                MessageBox.Show("This is NOT a valid integer" + Environment.NewLine + "(in the CATCH statement)");
+                MessageBox.Show("Please Try Again" + Environment.NewLine + "(Integers are whole numbers)");
+                textBox2.Clear();
+                status = false;
+            }
+            finally /*if the try succeeds, this will run*/
+            {
+                if (status == true)
+                {
+                    MessageBox.Show("This is a valid integer" + Environment.NewLine + "(in the FINALLY statement)" + Environment.NewLine + "needed to output a successful TRY");
+                }
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CW Exception Handling/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CW Exception Handling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ledcor Industries Inc.")]
+[assembly: AssemblyProduct("CW Exception Handling")]
+[assembly: AssemblyCopyright("Copyright © Ledcor Industries Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("840e3d59-c86c-4f5d-9393-d3cb162b6a89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
